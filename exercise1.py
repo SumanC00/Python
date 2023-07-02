78f8485871b21218ef8c5bfe68b6dc7a1256ca33
suman chapagai <sumanchapagai@gmail.com>
2023-07-02 17:04:14 -0400

    added python day one files

---
+++ exercise1.py
@@ -0,0 +1,3 @@
+list = ["one", "two", "three", "four"]
+my_variable = list
+print(list)
---
+++ main.py
@@ -0,0 +1,6 @@
+prompt = "Enter a to-do: "
+
+while True:
+    todo = input(prompt)
+    print(todo)
+    print("Next...")
