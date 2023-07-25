7f30a0c7621ab3e68c7bea4e557645f030618465
suman chapagai <sumanchapagai@gmail.com>
2023-07-25 12:28:02 -0400

    Python Day 3

---
+++ Exercise/exercise3.py
@@ -0,0 +1,9 @@
+country = "India"
+
+match country:
+    case "USA":
+        print("Helllo")
+    case "India":
+        print("Namaste")
+    case "Germany":
+        print("Hallo")
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-prompt = "Enter a to-do: "
-
 todos = []
 
 while True:
-    todo = input(prompt)
-    print(todo.capitalize())
-    todos.append(todo)
+    user_action = input("Type add, or show, or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'exit':
+            break
+        #case _:
+            #print("Hey, you entered an unknown command")
+
+print("Bye!")
